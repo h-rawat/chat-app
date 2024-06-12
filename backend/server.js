aafdcaabf35f454d5169e9d907ecb4d1f048ca54
Himanshu Rawat <himanshu.g.rawat@gmail.com>
2024-06-12 21:10:15 +0530

    refactor server.js and add middleware to parse incoming json payload

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,11 @@ import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+dotenv.config();
 
+app.use(express.json()); // to parse incoming requests with JSON payloads (from req.body)
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
