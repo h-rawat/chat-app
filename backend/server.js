40662f90ec108a37b2084e7b9fb3e9bdbb42dc67
Himanshu Rawat <himanshu.g.rawat@gmail.com>
2024-06-12 23:23:13 +0530

    setup send and get message routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -10,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json()); // to parse incoming requests with JSON payloads (from req.body)
+app.use(cookieParser()); // to access the cookies
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
