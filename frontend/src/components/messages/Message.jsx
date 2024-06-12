e8e156220744b2ed0ef8359e148abcb101820fb0
Himanshu Rawat <himanshu.g.rawat@gmail.com>
2024-06-13 01:49:01 +0530

    complete ui design

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,19 @@
+const Message = () => {
+  return (
+    <div className={`chat chat-end`}>
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img
+            src="https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
+            alt="Tailwind CSS chat bubble component"
+          />
+        </div>
+      </div>
+      <div className={`chat-bubble text-white pb-2`}>hey! whats up?</div>
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
+        10:00
+      </div>
+    </div>
+  );
+};
+export default Message;
