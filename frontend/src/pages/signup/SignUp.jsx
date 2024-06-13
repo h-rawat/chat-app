4679ec17fbca5fb71851134dfaa07ed2a2928604
Himanshu Rawat <himanshu.g.rawat@gmail.com>
2024-06-14 00:18:55 +0530

    setup react router dom

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import GenderCheckbox from "./GenderCheckbox";
 
 const SignUp = () => {
@@ -5,7 +6,7 @@ const SignUp = () => {
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
         <h1 className="text-3xl font-semibold text-center text-gray-300">
-          Login
+          SignUp
           <span className="text-blue-500"> ChatApp</span>
         </h1>
         <form>
@@ -52,12 +53,12 @@ const SignUp = () => {
 
           <GenderCheckbox />
 
-          <a
-            href="#"
+          <Link
+            to="/login"
             className="text-sm hover:underline hover:text-blue-500 mt-2 inline-block"
           >
             Already have an account?
-          </a>
+          </Link>
 
           <div>
             <button className="btn btn-block btn-sm mt-2">Signup</button>
