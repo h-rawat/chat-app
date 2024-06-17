d0acb860d17ee0f6b80c9e2565231699319a17ef
Himanshu Rawat <himanshu.g.rawat@gmail.com>
2024-06-17 18:58:58 +0530

    fix missing return keyword in  getMessages function of message controller

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -57,7 +57,7 @@ export const getMessages = async (req, res) => {
     }).populate("messages");
 
     if (!conversation) {
-      res.status(200).json([]);
+      return res.status(200).json([]);
     }
 
     const messages = conversation.messages;
